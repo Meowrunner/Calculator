5431e5e65938d02b47a981c59209300fd127dd41
Dmitry Gladyshev <dmitry@occipital.com>
2022-11-19 11:08:13 +0300

    Initial commit

---
+++ Calculat/CalculatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatApp.swift
+//  Calculat
+//
+//  Created by Dasha on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
